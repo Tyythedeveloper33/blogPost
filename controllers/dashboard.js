4469d53ce4bb641e54985d01c49092f2b9bdc9ff
unknown <kingwebbe@gmail.com>
2023-08-29 20:24:24 -0400

    trying to add relevant information for blogpost

--- controllers/dashboard.js
+++ controllers/dashboard.js
@@ -1,12 +1,33 @@
+const { BlogPost, User} = require('../models');
+
 const router = require('express').Router();
 
 
-router.get('/',(req,res) =>{
+router.get('/', async (req,res) =>{
+    try {
+        // Get all projects and JOIN with user data
+        const blogpostData = await BlogPost.findAll({
+          include: [
+            {
+              model: User,
+              attributes: ['name'],
+            },
+          ],
+        });
+    
+        // Serialize data so the template can read it
+        const blogPost = blogpostData.map((blogPost) => blogPost.get({ plain: true }));
     res.render('Dashboard', {
+          blogPost,
           style: "dashboard.css",
-
+          title: blogPost.title,
+          content: blogPost.content,
+          date_created: blogPost.date_created,
     });
-  
+  console.log(blogPost)
+    }catch (err) {
+        res.status(500).json(err);
+      }
 
     })
 
